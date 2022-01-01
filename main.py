83c5448a8a900e1e30d24ed9c2126786c8b74e5e
HMR1998 <humzahnmr@gmail.com>
2022-01-01 21:59:09 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+print("Welcome to the band name generator.")
+
+q1 = input("What is the name of the city you grew up in?")
+print(q1)
+
+q2 = input("What is your pet's name?")
+print(q2)
+
+band_name = q1 + q2
+print(f"The band name is {band_name}.")
